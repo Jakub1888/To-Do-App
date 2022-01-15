63564adcac15f9aabc9081cadb223e894f4c019e
Jakub1888 <jkrasnan1888@gmail.com>
2022-01-15 07:30:00 +0100

    register and login minor changes

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -5,8 +5,11 @@ namespace API.DTOs
     public class RegisterDto
     {
         [Required]
+        [RegularExpression(@"^[A-Za-z0-9]+$")]
         public string Username { get; set; }
+
         [Required]
+        [RegularExpression(@"^(?:(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*){6,20}$")]
         public string Password { get; set; }
     }
 }
